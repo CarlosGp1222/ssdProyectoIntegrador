e65373992c9b30673650751ada78f977e369c7cd
Havieru09 <jmurillor2@est.ups.edu.ec>
2023-08-17 00:12:15 -0500

    Añadida nuevas rutas

--- API_Matriculacion/src/routes/matriculacion.js
+++ API_Matriculacion/src/routes/matriculacion.js
@@ -4,27 +4,32 @@ const router = express.Router();
 const representantesController = require('../controllers/representantesController');
 const alumnoController = require('../controllers/alumnosController');
 const loginController = require('../controllers/loginController');
+const matriculaController = require('../controllers/matriculasController');
+const descuentoController = require('../controllers/descuentosController');
 
 //alumnos
 router.get('/alumno', alumnoController.list_all);
 router.get('/alumno/:cedula', alumnoController.list_cedula);
 router.post('/alumno', alumnoController.save);
-router.put('/:id_representante', alumnoController.update);
-
+router.put('/alumno/:cedula', alumnoController.update);
 //representantes
 router.get('/representante', representantesController.list_all);
 router.post('/representante', representantesController.save);
 router.put('/representante/:cedula', representantesController.update);
-
 //Login
 router.post('/login', loginController.list);
-
 //matricula
+router.get('/matricula', matriculaController.list_all);
+router.post('/matricula', matriculaController.save);
+router.put('/matricula/:id_matricula', matriculaController.update);
 //cursos
 //facturas
 //nota
 //recibo
 //descuento
+router.get('/descuento', descuentoController.list_all);
+router.post('/descuento', descuentoController.save);
+router.put('/descuento/:id_descuento', descuentoController.update);
 //metodos
 
 
